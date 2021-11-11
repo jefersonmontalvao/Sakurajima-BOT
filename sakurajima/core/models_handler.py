d46c8db515f3c90706dfe62ab38f9e70ad7318d5
Jeferson Montalvão <76631154+jefersonmontalvao@users.noreply.github.com>
2021-11-11 01:02:27 -0300

    update in models_handler.py file
    Operations class for sqlite3 backend was created and some updates on ModelsHandler class.

--- sakurajima/core/models_handler.py
+++ sakurajima/core/models_handler.py
@@ -10,22 +10,28 @@ class ModelsHandler:
     def __init__(self):
         # Define few variables.
         self.backend = None
+        self.schema_data = None
 
         # Set the database backend module.
+        # Anyway, it set the "backend" variable a instance of a backend module
+        # that is in "sakurajima.db.backends" module.
         self.set_db_backend()
 
         # Set the values of schema_data property.
         self.import_models()
 
+        print(self.backend.schema.SchemaAdmin())
+        print(self.backend.operations.Operations())
+
+
     @property
-    def schema_data(self):
+    def schema_data(self) -> tuple:
         """
         Property get of data of data.
         """
-        try:
-            value = getattr(self.__class__, '__schema_data')
-        except AttributeError:
-            setattr(self.__class__, '__schema_data', [])
+        value = getattr(self.__class__, '__schema_data')
+        if value is None:
+            setattr(self.__class__, '__schema_data', ())
             value = getattr(self.__class__, '__schema_data')
         return value
 
@@ -36,6 +42,11 @@ class ModelsHandler:
         """
         if isinstance(value, (list, tuple)):
             setattr(self.__class__, '__schema_data', tuple(value))
+        elif value is None:
+            setattr(self.__class__, '__schema_data', value)
+        else:
+            # TODO: Implement this later.
+            pass
 
     def set_db_backend(self):
         """
@@ -66,7 +77,7 @@ class ModelsHandler:
         module_components = tuple(filter(lambda comp: not re.search('^__.+__$', comp), dir(models)))
 
         # Variable to store models instance that is found on module components to return it.
-        models_instances = list()
+        models_data_repr_list = list()
 
         for component in module_components:
             # Get the component object:
@@ -76,6 +87,13 @@ class ModelsHandler:
                 # Instantiate the model class.
                 # Is important to say that class return a dictionary with some data
                 # about the database model(name, fields...).
-                models_instances.append(ModelTemplateObject())
+                instance = ModelTemplateObject()
+                models_data_repr_list.append(instance)
 
-        self.schema_data = tuple(models_instances)
+        self.schema_data = tuple(models_data_repr_list)
+
+    def initialize_database_models(self):
+        # TODO: Check if the database has the models
+        # TODO: If not, create the models in database
+        # TODO: else if has the model but with modifications, use alter table to alter this model in database.
+        pass
---
+++ sakurajima/db/backends/sqlite3/operations.py
@@ -0,0 +1,6 @@
+from db.backends.base.operations import BaseOperations
+
+
+class Operations(BaseOperations):
+    def __init__(self):
+        super(Operations, self).__init__()
